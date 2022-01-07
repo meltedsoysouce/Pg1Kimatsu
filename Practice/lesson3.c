03af124016a4406ffaa762df45f3c22c248b638b
meltedsoysource <metalsngke@gmail.com>
2022-01-07 19:36:04 +0900

    make lesson3

---
+++ Practice/lesson3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+double CalcSumSquare(double x, double y)
+{
+    return (x*x) + (y*y);
+}
+
+int main(void)
+{
+    double x=0.0, y=0.0;
+
+    printf("x -> ");
+    scanf("%lf", &x);
+    
+    printf("y -> ");
+    scanf("%lf", &y);
+
+    printf("x^2 + y^2 = %f\n", CalcSumSquare(x, y));
+
+    return 0;
+}
